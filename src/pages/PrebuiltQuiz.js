fa3bb3464ab9f71ef249df71c945651358de7595
Brittany Starr <lunarunar@gmail.com>
2016-12-10 18:33:06 -0700

    built out GetQuestions and clicked functions for PrebuiltQuiz

--- src/pages/PrebuiltQuiz.js
+++ src/pages/PrebuiltQuiz.js
@@ -9,28 +9,36 @@ export default class PrebuiltQuiz extends React.Component {
       correct: '',
       wrong1: '',
       wrong2: '',
-      wrong3: ''
+      wrong3: '',
+      questions: [],
+      answers: []
     };
   }
 
   componentDidMount(){
+    this.GetQuestions()
+  }
+
+  GetQuestions() {
+    var questions;
     axios.get('/questions')
       .then(response =>{
+        questions = response.data;
+        // console.log(questions)
         // new var - get response.data arr
         // iterate through for each question
           // get index 1 to end
             // shuffle the values and then map to
           // onclick would get the value of the clicked button
             // compare that value to this.state.correct
-
         this.setState({
-          name: response.data[0].name,
-          correct: response.data[0].correct,
-          wrong1: response.data[0].wrong1,
-          wrong2: response.data[0].wrong2,
-          wrong3: response.data[0].wrong3
-        }, function(){
-          console.log(this.state.correct)
+          name: questions[0].name,
+          correct: questions[0].correct,
+          wrong1: questions[0].wrong1,
+          wrong2: questions[0].wrong2,
+          wrong3: questions[0].wrong3,
+          questions: this.state.questions.concat(questions),
+          answers: this.state.answers.concat(questions[0].correct, questions[0].wrong1, questions[0].wrong2, questions[0].wrong3)
         })
       })
       .catch(function(err){
@@ -38,19 +46,20 @@ export default class PrebuiltQuiz extends React.Component {
       })
   }
 
+  clicked(e) {
+    if (this.state.correct === e.target.textContent) {
+      alert("Correct");
+    } else {
+      alert("Wrong");
+    }
+  }
+
   render() {
-    // console.log("PrebuiltQuiz");
-
+    // getting this in with the componentDidMount
     return (
-
        <div className="App">
-        <h1>PrebuiltQuiz Page</h1>
-        <div className="container">
-          <div className="buttons">
-            <button className="answer">{this.state.correct}</button><br />
-            <button className="answer">{this.state.wrong1}</button>
-          </div>
-        </div>
+        {this.state.answers.map(option => <button onClick={this.clicked.bind(this)} className={`answer btn btn-lg ${option}`}>{option}</button> )}
+        <div className="container"></div>
       </div>
     );
   }
