05b62e43cd119891388536e42408dfd5fb72dc6b
prithvi_anantharaj <prithvi.anantharaj@gmail.com>
2016-12-12 10:18:06 -0800

    Homepage

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -26,10 +26,19 @@ export default class Nav extends React.Component {
 
       <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">CrashCourse</a>
+          </div>
           <div className={"navbar-collapse " + navClass} id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav">
+            <ul className="nav navbar-nav navbar-right">
 
-                <Link to="settings" onClick={this.toggleCollapse.bind(this)}>Settings</Link>
+                <li><span className="settings"> <Link to="settings" onClick={this.toggleCollapse.bind(this)}>Settings</Link></span> </li>
                 <li><span className="username">USERNAME</span></li>
 
             </ul>
---
+++ src/pages/Homepage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class Homepage extends React.Component {
+  render() {
+    console.log("homepage");
+    return (
+      <h1>homepage</h1>
+    );
+  }
+}
