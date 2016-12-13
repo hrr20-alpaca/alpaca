64de6cc1078035ce45c03ada8823f6b7741e180e
prithvi_anantharaj <prithvi.anantharaj@gmail.com>
2016-12-12 16:42:54 -0800

    HomePage and React-router links are fixed.

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Link } from 'react-router'
 
 export default class Homepage extends React.Component {
   render() {
     console.log("homepage");
     return (
-      <h1>homepage</h1>
+      <div className="container homepage">
+        <div className="row text-center hero-section">
+          <h1>CrashCourse</h1>
+          <i className="fa fa-desktop"></i>
+        </div>
+        <div className="row main-content">
+          <div className="col-md-4 text-left">
+              <h2>What is CrashCourse?</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+          </div>
+          <div className="col-md-4 text-left">
+              <h2>Why use CrashCourse?</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+          </div>
+          <div className="col-md-4 text-left">
+              <h2>Get started</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+              <Link to="/signup">Learn More</Link>
+          </div>
+        </div>
+      </div>  
     );
   }
 }
